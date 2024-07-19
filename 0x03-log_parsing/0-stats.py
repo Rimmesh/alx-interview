4a71e04e4c27ab11540567d2fa3bb8ad24eebe5e
Rimmesh <rimmeshete@gmail.com>
2024-07-19 14:50:50 -0700

    commited

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,58 @@
 #!/usr/bin/python3
+"""Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
-<IP Address> - [<date>]
-"GET /projects/260 HTTP/1.1" <status code>
-"""
 
 
-import signal
-import sys
-import re
-import time
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
 
-regex = '^([0-9]?[0-9]?[0-9]\.){3}[0-9]?[0-9]?[0-9] \
-- \[\S* \S*] "\S* \S* \S*" [2-5]0[0,1,3,4,5] [0-9]+$'
-ipregex = '^([0-9]?[0-9]?[0-9]\.){3}[0-9]?[0-9]?[0-9]'
-filesizeregex = '[0-9]+$'
-statuscoderegex = '" [2-5]0[0,1,3,4,5]'
-valid_status_code = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-    }
-filesize = 0
-lineindex = 0
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
+if __name__ == "__main__":
+    import sys
 
-def show():
-    """display status"""
-    print("File size: {}".format(filesize))
-    for code, num in valid_status_code.items():
-        if num:
-            print("{}: {}".format(code, num))
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
-try:
-    for line in sys.stdin:
-        lineindex += 1
-        if re.match(regex, line):
-            statuscode = re.search(statuscoderegex, line).group(0)[2:]
-            filesize += int(re.search(filesizeregex, line).group(0))
-            if statuscode in valid_status_code:
-                valid_status_code[statuscode] += 1
-        if lineindex % 10 == 0:
-            show()
-except Exception:
-    pass
-finally:
-    show()
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
